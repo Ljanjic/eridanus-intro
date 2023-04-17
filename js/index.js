d5d5af56f1f2849e8b89f9c8979bb618131792fb
Ljiljana <ljiljajanjic.com>
2023-04-17 22:35:38 +0200

    Working with Fetch API

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 //added a new date object and stored in a var "today"
 let today = new Date();
-
-
 //Got the current year from new Date object and stored into thisYear
 let thisYear = today.getFullYear();
 
@@ -18,39 +16,39 @@ let skills = ["* Communication", "* Front-end Coding", "* Git", "* Problem Solvi
 let skillsSection = document.getElementById("skills");
 let skillsList = skillsSection.querySelector("ul");
 for (let i = 0; i < skills.length; i++) {
-    let skill = document.createElement("li")
-     skill.innerHTML = skills[i]
-     skillsList.appendChild(skill)
+	let skill = document.createElement("li")
+	skill.innerHTML = skills[i]
+	skillsList.appendChild(skill)
 }
 
 //Handling Message Form Submit
 const messageForm = document.forms.leave_message;
 messageForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const user = event.target.usersName.value;
-    const mail = event.target.usersEmail.value;
-    const mssg = event.target.usersMessage.value;
-console.log(user, mail, mssg);
-let messageSection = document.getElementById("messages");
-let messageList = messageSection.querySelector("ul");
-let newMessage = document.createElement("li");
-newMessage.innerHTML = `<a href=mailto:${mail}>${user}'s message:</a> <span>"${mssg}"</span>`; 
-let removeButton = document.createElement("button");
-removeButton.className = "button_class";
-removeButton.innerText = "remove";
-removeButton.type = "button";
- removeButton.addEventListener('click', (event) => {
-   const entry = event.target.parentNode;
-    entry.remove()  
-    if (messageList.childElementCount === 0) {
-        messageSection.style.display = "none";
-    }  
- }) 
- messageSection.style.display = "block";
- newMessage.appendChild(removeButton);
-     messageList.append(newMessage);
+	event.preventDefault();
+	const user = event.target.usersName.value;
+	const mail = event.target.usersEmail.value;
+	const mssg = event.target.usersMessage.value;
+	console.log(user, mail, mssg);
+	let messageSection = document.getElementById("messages");
+	let messageList = messageSection.querySelector("ul");
+	let newMessage = document.createElement("li");
+	newMessage.innerHTML = `<a href=mailto:${mail}>${user}'s message:</a> <span>"${mssg}"</span>`;
+	let removeButton = document.createElement("button");
+	removeButton.className = "button_class";
+	removeButton.innerText = "remove";
+	removeButton.type = "button";
+	removeButton.addEventListener('click', (event) => {
+		const entry = event.target.parentNode;
+		entry.remove()
+		if (messageList.childElementCount === 0) {
+			messageSection.style.display = "none";
+		}
+	})
+	messageSection.style.display = "block";
+	newMessage.appendChild(removeButton);
+	messageList.append(newMessage);
 });
-//Fetch GitHub Repositories
+/*Fetch GitHub Repositories
     let gitHubRequest = new XMLHttpRequest();
     gitHubRequest.open("GET", "https://api.github.com/users/Ljanjic/repos");
     gitHubRequest.send();
@@ -70,4 +68,29 @@ for (let i = 0; i < repositories.length; i++) {
 }
   projectList.appendChild(project);
 }
-})
+}) */
+
+//Fetch API
+fetch('https://api.github.com/users/Ljanjic/repos')
+	.then(response => {
+		return response.json();
+	})
+	.then(repositories => {
+		console.log(repositories);
+		//Display Repositories in List
+		let projectSection = document.getElementById("projects");
+		let projectList = projectSection.querySelector("ul");
+		for (let i = 0; i < repositories.length; i++) {
+			let project = document.createElement("li");
+			if (repositories[i].name === 'eridanus-intro') {
+				project.innerHTML = `<a href = "${repositories[i].html_url}">${repositories[i].name}</a>`
+			} else {
+				project.innerText = repositories[i].name;
+			}
+			projectList.appendChild(project);
+		}
+	})
+	.catch(error => {
+		console.log('You made some error!');
+		console.error(error);
+	});
